03309c01342179e87fcfd4f0534606fae0c29caf
robgor3 <gorkiewicz.r@gmail.com>
2023-11-22 22:17:30 +0100

    chore: add @angular/material, add prettier, tweak config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+// TODO remove if unused
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [provideRouter(appRoutes), provideAnimations()],
 };
