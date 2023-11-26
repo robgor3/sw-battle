e9f040a0309541f787d181fd26b08bb2ade618b1
robgor3 <gorkiewicz.r@gmail.com>
2023-11-26 18:51:07 +0100

    feat: action buttons styling

--- modules/ui/src/lib/button/button.component.ts
+++ modules/ui/src/lib/button/button.component.ts
@@ -6,9 +6,11 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [MatButtonModule],
   selector: 'sw-battle-ui-button',
   templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent {
   @Input({ required: true }) public text: string = '';
   @Input() public color: string = '';
+  @Input() public fullWidth: boolean = false;
 }
